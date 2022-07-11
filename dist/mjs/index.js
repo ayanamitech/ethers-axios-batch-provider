627dc648b183eb9b0dea048589cd92491811bd83
Ayanami <ayanami0330@protonmail.com>
2022-07-11 20:00:54 +0900

    Bump axios-auto version ( 5.6.16 )
    Fixed the use of AbortController

--- dist/mjs/index.js
+++ dist/mjs/index.js
@@ -25,7 +25,7 @@
 import { post } from 'axios-auto';
 import { utils, providers } from 'ethers';
 
-const version = "ethers-axios-batch-provider@5.6.15";
+const version = "ethers-axios-batch-provider@5.6.16";
 
 const logger = new utils.Logger(version);
 class AxiosBatchProvider extends providers.JsonRpcProvider {
--- types/_version.d.ts
+++ types/_version.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "ethers-axios-batch-provider@5.6.15";
+export declare const version = "ethers-axios-batch-provider@5.6.16";
